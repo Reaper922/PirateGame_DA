716cbbb47495742b75e94c462da9562075ebe4da
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-25 22:53:40 +0100

    Position style moved to css

--- js/script.js
+++ js/script.js
@@ -9,13 +9,12 @@ function init() {
 addEventListener('mousemove', (event) => {
     const cursor = document.getElementById('cursor');
 
-    cursor.style.position = 'fixed';
     cursor.style.top = `${event.y.toFixed().toString()}px`;
     cursor.style.left = `${event.x.toFixed().toString()}px`;
 })
 
 
-addEventListener('contextmenu', (event) => event.preventDefault())
+// addEventListener('contextmenu', (event) => event.preventDefault())
 
 
 init();
