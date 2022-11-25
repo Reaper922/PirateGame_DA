9e6ec7fa0d458707d27f9914477501a0b81992b1
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-25 22:55:51 +0100

    Reworked scene class and added render function

--- js/classes/scene.class.js
+++ js/classes/scene.class.js
@@ -1,14 +1,62 @@
 import { Player } from "./player.class.js";
+import { Sprite } from "./sprite.class.js";
+import { layerData, terrainData } from './settings.js';
 
 export class Scene {
-    constructor(ctx, startLevel = 1) {
+    constructor(ctx, level = 1) {
         this.ctx = ctx;
-        this.player = new Player(ctx, '/assets/sprites/player/idle_right/1.png');
-        this.background = [];
-        this.foreground = [];
-        this.terrain = [];
-        this.enemies = [];
-        this.collectables = [];
+        this.currentLevel = level;
+        this.levelData = {}
+        this.layers = layerData;
+        this.player = new Player(ctx);
+        // this.enemies = [];
+        // this.collectables = [];
+        this.loadLayerData();
+        this.loadLayerSprites();
+    }
+
+    async loadLayerData() {
+        const levelPath = `../../level/level${this.currentLevel}.json`;
+        this.levelData = await fetch(levelPath).then((resp) => resp.json());
+            
+        for (const layer in this.layers) {
+            const currentLayer = this.layers[layer];
+            currentLayer.data = this.levelData.layers[currentLayer.id].data;
+        }
+    }
+
+    loadLayerSprites() {
+        for (const layer in this.layers) {
+            const currentLayer = this.layers[layer];
+
+            for (let i = 0; i < currentLayer.numSprites; i++) {
+                const sprite = new Sprite(`${currentLayer.path}/${i}.png`).image;
+                currentLayer.sprites.push(sprite);
+            }
+        }
+    }
+
+    renderLayer() {
+        const levelWidth = this.levelData.width;
+        const levelHeigth = this.levelData.height;
+        const tileWidth = this.levelData.tileWidth;
+        const tileHeight = this.levelData.tileHeight;
+        
+        // for (const layer in this.layers) {
+            const currentLayer = this.layers.terrain;
+            let row = 0;
+
+            for (let i = 0; i < currentLayer.data.length; i++) {
+                let col = i % levelWidth;
+
+                if (currentLayer.data[i] != 0) {
+                    let id = terrainData.spritesIds[currentLayer.data[i]];
+                    this.ctx.drawImage(currentLayer.sprites[id], col * 64, row * 64);
+                }
+        
+                if (col === (15)) {row += 1}
+            }
+        // }
     }
 
     update() {
@@ -16,6 +64,7 @@ export class Scene {
     }
 
     render() {
+        this.renderLayer();
         this.player.render();
     }
 }
