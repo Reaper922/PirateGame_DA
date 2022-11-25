cdb865f52e68d9268d14cfc71de99608fff20e33
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-25 22:54:23 +0100

    Supplemented settings

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -4,3 +4,55 @@ export const window = {
 }
 
 export const playerSpeed = 5;
+
+export const layerData = {
+    sky: {
+        id: 0,
+        path: '/assets/sprites/terrain/clouds',
+        data: [],
+        sprites: [],
+        numSprites: 3
+    },
+    background: {
+        id: 1,
+        path: '/assets/sprites/terrain/palm/bg',
+        data: [],
+        sprites: [],
+        numSprites: 16
+    },
+    terrain: {
+        id: 2,
+        path: '/assets/sprites/terrain/land',
+        data: [],
+        sprites: [],
+        numSprites: 53
+    },
+    foreground: {
+        id: 3,
+        path: '/assets/sprites/terrain/palm/fg',
+        data: [],
+        sprites: [],
+        numSprites: 16
+    }
+}
+
+export const terrainData = {
+    spritesIds: {
+        41: 1,
+        42: 2,
+        43: 3,
+        44: 4,
+        45: 5,
+        46: 6,
+        47: 7,
+        79: 39,
+        83: 43,
+        84: 44,
+        87: 47,
+        88: 48,
+        90: 50,
+        92: 52,
+
+    }
+}
+
