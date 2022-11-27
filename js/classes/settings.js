7e209c56c1b99af10e399ee56e1425d92714f0a1
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-27 23:11:29 +0100

    Adjusted settings

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -4,7 +4,44 @@ export const window = {
 }
 
 export const gravity = 0.4;
-export const playerSpeed = 5;
+
+export const playerData = {
+    speed: 5,
+    animations: {
+        idle_left: {
+            path: '\assets\sprites\player\idle_left',
+            numSprites: 5
+        },
+        idle_right: {
+            path: '\assets\sprites\player\idle_right',
+            numSprites: 5
+        },
+        run_left: {
+            path: '\assets\sprites\player\run_left',
+            numSprites: 6
+        },
+        run_right: {
+            path: '\assets\sprites\player\run_right',
+            numSprites: 6
+        },
+        jump_left: {
+            path: '\assets\sprites\player\jump_left',
+            numSprites: 1
+        },
+        jump_right: {
+            path: '\assets\sprites\player\jump_right',
+            numSprites: 1
+        },
+        fall_left: {
+            path: '\assets\sprites\player\fall_left',
+            numSprites: 1
+        },
+        fall_right: {
+            path: '\assets\sprites\player\fall_right',
+            numSprites: 1
+        },
+    }
+}
 
 export const layerData = {
     sky: {
@@ -12,48 +49,31 @@ export const layerData = {
         path: '/assets/sprites/terrain/clouds',
         data: [],
         sprites: [],
-        numSprites: 3
+        numSprites: 3,
+        spriteOffset: 0
     },
     background: {
         id: 1,
         path: '/assets/sprites/terrain/palm/bg',
         data: [],
         sprites: [],
-        numSprites: 16
+        numSprites: 16,
+        spriteOffset: 0
     },
     terrain: {
         id: 2,
         path: '/assets/sprites/terrain/land',
         data: [],
         sprites: [],
-        numSprites: 53
+        numSprites: 53,
+        spriteOffset: -40,
     },
     foreground: {
         id: 3,
         path: '/assets/sprites/terrain/palm/fg',
         data: [],
         sprites: [],
-        numSprites: 16
+        numSprites: 16,
+        spriteOffset: 0
     }
 }
-
-export const terrainData = {
-    spritesIds: {
-        41: 1,
-        42: 2,
-        43: 3,
-        44: 4,
-        45: 5,
-        46: 6,
-        47: 7,
-        79: 39,
-        83: 43,
-        84: 44,
-        87: 47,
-        88: 48,
-        90: 50,
-        92: 52,
-
-    }
-}
-
