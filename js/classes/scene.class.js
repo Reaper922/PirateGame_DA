86e0c1853829dfa29fba69a2ac401016c990555d
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-02 01:19:44 +0100

    Several adjustments

--- js/classes/scene.class.js
+++ js/classes/scene.class.js
@@ -1,18 +1,25 @@
 import { Player } from "./player.class.js";
 import { Sprite } from "./sprite.class.js";
 import { layerData } from './settings.js';
+import { Terrain } from "./terrain.class.js";
 
 export class Scene {
-    constructor(ctx, frameCounter, level = 1) {
+    constructor(ctx, level = 1) {
         this.ctx = ctx;
         this.currentLevel = level;
         this.levelData = {}
         this.layers = layerData;
-        this.player = new Player(ctx, frameCounter, 50, 300, 44, 56);
+        this.player = new Player(ctx, 50, 300);
+        this.terrain = [];
         // this.enemies = [];
         // this.collectables = [];
-        this.loadLayerData();
+        this.init();
+    }
+
+    async init() {
+        await this.loadLayerData();
         this.loadLayerSprites();
+        this.createTerrain();
     }
 
     async loadLayerData() {
@@ -25,7 +32,7 @@ export class Scene {
         }
     }
 
-    loadLayerSprites() {
+    loadLayerSprites() { // Mit implementierung von Animationen in die jeweilige Klasse verlagern -> Player/Collectables/Foreground
         for (const layer in this.layers) {
             const currentLayer = this.layers[layer];
 
@@ -36,35 +43,56 @@ export class Scene {
         }
     }
 
-    renderLayer() {
+    createTerrain() {
+        const terrainData = this.levelData.layers[2].data;
+        const levelWidth = this.levelData.width;
+        const levelHeigth = this.levelData.height;
+        const tileWidth = this.levelData.tileWidth;
+        const tileHeight = this.levelData.tileHeight;
+        let row = 0;
+
+        for (let i = 0; i < terrainData.length; i++) {
+            let col = i % levelWidth;
+
+            if (terrainData[i] != 0) {
+                this.terrain.push(new Terrain(this.ctx, col * 64, row * 64, 64, 64))
+            }
+
+            if (col === (15)) {row += 1}
+        }
+    }
+
+    renderLayers() { // Umschreiben, sodass die Renderfunktion der Objekte aufgerufen wird.
         const levelWidth = this.levelData.width;
         const levelHeigth = this.levelData.height;
         const tileWidth = this.levelData.tileWidth;
         const tileHeight = this.levelData.tileHeight;
         
-        // for (const layer in this.layers) {
-            const currentLayer = this.layers.terrain;
+        for (const layer in this.layers) {
+            const currentLayer = this.layers[layer];
             let row = 0;
+            if (layer === 'foreground' || layer === 'background') {row -= 1}
 
             for (let i = 0; i < currentLayer.data.length; i++) {
                 let col = i % levelWidth;
 
                 if (currentLayer.data[i] != 0) {
-                    let id = currentLayer.data[i] + this.layers.terrain.spriteOffset;
+                    // console.log(layer, currentLayer.data[i])
+                    let id = currentLayer.data[i] + currentLayer.spriteOffset;
                     this.ctx.drawImage(currentLayer.sprites[id], col * 64, row * 64);
                 }
         
                 if (col === (15)) {row += 1}
             }
-        // }
+        }
     }
 
     update() {
-        this.player.update();
+        this.player.update(this.terrain);
     }
 
     render() {
-        this.renderLayer();
+        this.renderLayers();
         this.player.render();
     }
 }
