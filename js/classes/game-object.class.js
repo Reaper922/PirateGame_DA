cde6824363a2ae0e2759c38e523f031509568316
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-02 01:19:17 +0100

    Moved collision handling from player

--- js/classes/game-object.class.js
+++ js/classes/game-object.class.js
@@ -18,11 +18,65 @@ export class GameObject {
         }
     }
 
+    move() {
+        this.position.x += this.velocity.x;
+    }
+
     addGravity() {
         if (this.velocity.y < 10 && !this.isStatic) {
             this.velocity.y += gravity;
         }
         this.position.y += Number(this.velocity.y.toFixed(1));
+    }
+
+    checkCollisions(direction, arr) {
+        for (let i = 0; i < arr.length; i++) {
+            const tile = arr[i];
+            const collision = {
+                top: this.position.y < tile.position.y + tile.height,
+                bottom: this.position.y + this.height > tile.position.y,
+                left: this.position.x < tile.position.x + tile.width,
+                right: this.position.x + this.width > tile.position.x
+            }
+
+            if (collision.top && collision.bottom && collision.left && collision.right) {
+                switch (direction) {
+                    case 'horizontal':
+                        this.handleHorizontalCollision(tile);
+                        break;
+                    case 'vertical':
+                        this.handleVerticalCollision(tile);
+                        break;
+                }
+            }
+        }
+    }
+
+    handleHorizontalCollision(tile) {
+        // Right Collision
+        if (this.velocity.x > 0) {
+            this.position.x = tile.position.x - this.width;
+        }
+
+        // Left Collision
+        if (this.velocity.x < 0) {
+            this.position.x = tile.position.x + tile.width;
+        }
+    }
+
+    handleVerticalCollision(tile) {
+        // Bottom Collision
+        if (this.velocity.y > 0) {
+            this.position.y = tile.position.y - this.height;
+            this.canJump = true;
+            this.velocity.y = 0;
+        }
+
+        // Top Collision
+        if (this.velocity.y < 0) {
+            this.position.y = tile.position.y + tile.height;
+            this.velocity.y = 0;
+        }
     }
 
     update() {
