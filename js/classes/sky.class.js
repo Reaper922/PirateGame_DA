8c46f439b324a0ae4b8929f73d78e4ab4e6921ec
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-06 23:11:08 +0100

    Added sky movement

--- js/classes/sky.class.js
+++ js/classes/sky.class.js
@@ -1,8 +1,33 @@
 import { GameObject } from "./game-object.class.js";
+import { window } from './settings.js';
+
 
 export class Sky extends GameObject {
     constructor(ctx, levelData) {
         super(ctx, levelData);
         this.createLayer('sky');
+        // this.speedSmall = 1;
+        // this.speedMedium = 4;
+        // this.speedBig = 6;
+    }
+
+    move() {
+        for (let i = 0; i < this.sprites.length; i++) {
+            const currentSprite = this.sprites[i];
+            currentSprite.position.x -= 0.1;
+        }
+    }
+
+    resetPosition() {
+        for (let sprite of this.sprites) {
+            if (sprite.position.x < 0 - sprite.image.width) {
+                sprite.position.x = window.width;
+            }
+        }
+    }
+
+    update() {
+        this.resetPosition();
+        this.move();
     }
 } 
