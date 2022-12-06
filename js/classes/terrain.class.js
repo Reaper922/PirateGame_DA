95466b1f0957d765a87d0ac5a6419061c431407c
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-06 22:36:35 +0100

    Modified terrain class

--- js/classes/terrain.class.js
+++ js/classes/terrain.class.js
@@ -1,12 +1,8 @@
 import { GameObject } from "./game-object.class.js";
 
 export class Terrain extends GameObject {
-    constructor(ctx, positionX, positionY, width, height) {
-        super('/assets/sprites/terrain/land/1.png');
-        this.ctx = ctx;
-        this.position.x = positionX;
-        this.position.y = positionY;
-        this.width = width;
-        this.height = height;
+    constructor(ctx, levelData) {
+        super(ctx, levelData);
+        this.createLayer('terrain');
     }
-}
+} 
