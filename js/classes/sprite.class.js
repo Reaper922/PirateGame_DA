e9539e258ac4dc9b78ece17285fc23347284ab01
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-06 19:43:34 +0100

    Added position to sprite

--- js/classes/sprite.class.js
+++ js/classes/sprite.class.js
@@ -2,6 +2,10 @@ export class Sprite {
     constructor(path) {
         this.image = new Image();
         this.setSprite(path);
+        this.position = {
+            x: 0,
+            y: 0
+        }
     }
 
     setSprite(path) {
