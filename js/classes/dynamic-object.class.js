7d75728a56353c9d3eef14a0d11c7de5dcc0586d
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-08 23:02:41 +0100

    Refactored game object class

---
+++ js/classes/dynamic-object.class.js
@@ -0,0 +1,120 @@
+import { gravity } from "./settings.js";
+import { StaticObject } from "./static-object.class.js";
+
+/**
+ * Extends the root class with movement, gravity and collision methods.
+ */
+export class DynamicObject extends StaticObject {
+    constructor(ctx, layerData, position) {
+        super(ctx, layerData, position);
+        this.isStatic = false;
+    }
+
+    /**
+     * Moves the object on the x-axis based on the x-velocity.
+     */
+    move() {
+        this.position.x += this.velocity.x;
+    }
+
+    /**
+     * Moves the object downwards on the y-axis based on the gravity value.
+     */
+    addGravity() {
+        if (this.velocity.y < 10 && !this.isStatic) {
+            this.velocity.y += gravity;
+        }
+        this.position.y += Number(this.velocity.y.toFixed(1));
+    }
+
+    /**
+     * Checks if the current object is colliding with a sprite from the array.
+     * @param {Array} spritesArray Array of Sprites.
+     * @param {String} type Type of collision.
+     * @param {Object} collisionObject Object that checks the collision.
+     */
+    checkCollision(spritesArray, type, collisionObject = null) {
+        for (let i = 0; i < spritesArray.length; i++) {
+            const sprite = spritesArray[i];
+            const collision = {
+                top: this.position.y < sprite.position.y + sprite.image.height,
+                bottom: this.position.y + this.height > sprite.position.y,
+                left: this.position.x < sprite.position.x + sprite.image.width,
+                right: this.position.x + this.width > sprite.position.x
+            }
+
+            if (collision.top && collision.bottom && collision.left && collision.right) {
+                this.handleCollisionType(sprite, type, collisionObject);
+            }
+        }
+    }
+
+    /**
+     * Decides how the collision is handled based on its type.
+     * @param {Sprite} sprite Sprite object.
+     * @param {String} type Type of collision.
+     * @param {Object} collisionObject Object that checks the collision.
+     */
+    handleCollisionType(sprite, type, collisionObject) {
+        switch (type) {
+            case 'horizontal':
+                this.handleHorizontalCollision(sprite);
+                break;
+            case 'vertical':
+                this.handleVerticalCollision(sprite);
+                break;
+            case 'water':
+                this.handleWaterCollision(collisionObject);
+        }
+    }
+
+    /**
+     * Handles the collisions in horizontal direction.
+     * @param {Sprite} sprite Sprite object
+     */
+    handleHorizontalCollision(sprite) {
+        // Right Collision
+        if (this.velocity.x > 0) {
+            this.position.x = sprite.position.x - this.width;
+        }
+
+        // Left Collision
+        if (this.velocity.x < 0) {
+            this.position.x = sprite.position.x + sprite.image.width;
+        }
+    }
+
+    /**
+     * Handles the collisions in vertical direction.
+     * @param {Sprite} sprite Sprite object
+     */
+    handleVerticalCollision(sprite) {
+        // Bottom Collision
+        if (this.velocity.y > 0) {
+            this.position.y = sprite.position.y - this.height;
+            this.canJump = true;
+            this.velocity.y = 0;
+        }
+
+        // Top Collision
+        if (this.velocity.y < 0) {
+            this.position.y = sprite.position.y + sprite.image.height;
+            this.velocity.y = 0;
+        }
+    }
+
+    /**
+     * Handles the collision with water.
+     * @param {Object} collisionObject Object that checks the collision.
+     */
+    handleWaterCollision(collisionObject) {
+        collisionObject.waterCollision();
+    }
+
+    /**
+     * Updates the game object.
+     */
+    update() {
+        this.addGravity();
+    }
+}
---
+++ js/classes/static-object.class.js
@@ -0,0 +1,46 @@
+/**
+ * Root object of all game objects.
+ */
+export class StaticObject {
+    constructor(ctx, layerData) {
+        this.ctx = ctx;
+        this.layerData = layerData;
+        this.sprites = [];
+        this.width = 0;
+        this.height = 0;
+        this.position = {
+            x: 0,
+            y: 0
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    /**
+     * Updates the game object.
+     */
+    update() {
+    }
+
+    /**
+     * Renders the sprites of the game object.
+     */
+    render() {
+        for (let i = 0; i < this.sprites.length; i++) {
+            this.ctx.drawImage(this.sprites[i].image, this.sprites[i].position.x, this.sprites[i].position.y);
+        }
+    }
+
+    /**
+     * Debugging function that renders a red outline of the objects boundary.
+     */
+    drawRect() {
+        this.ctx.beginPath();
+        this.ctx.lineWidth = "2";
+        this.ctx.strokeStyle = 'red';
+        this.ctx.rect(this.position.x, this.position.y, this.width, this.height);
+        this.ctx.stroke();
+    }
+}
