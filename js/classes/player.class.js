f174a366b7e927e9d6407e51c5ec95e6a7ba5d16
Dennis Ammen <dennis.ammen@outlook.com>
2022-12-08 01:13:42 +0100

    Refactored player class and added JsDoc comments

--- js/classes/player.class.js
+++ js/classes/player.class.js
@@ -3,30 +3,40 @@ import { InputController } from "./input-controller.class.js";
 import { playerData, window } from './settings.js';
 import { Sprite } from "./sprite.class.js";
 
+/**
+ * Class that represents the player.
+ */
 export class Player extends GameObject {
-    constructor(ctx, position) {
-        super();
+    constructor(ctx, layerData, position = { x: 0, y: 0 }) {
+        super(ctx, layerData);
         this.ctx = ctx;
         this.inputController = new InputController();
-        this.position.x = position.x;
-        this.position.y = position.y;
+        this.startPosition = position;
+        this.position = {
+            x: position.x,
+            y: position.y
+        }
         this.width = playerData.width;
         this.height = playerData.height;
         this.speed = playerData.speed;
+        this.health = playerData.health;
         this.jumpVelocity = playerData.jumpVelocity;
+        this.canJump = false;
+        this.isStatic = false;
         this.animations = [];
         this.currentAnimation = 'idle_right';
         this.animationFrame = 0;
         this.staggerFrames = playerData.animationSpeed;
         this.isLastInputRight = true;
-        this.canJump = false;
         this.isAttacking = false;
         this.attackCooldown = false;
-        this.isStatic = false;
 
         this.loadAnimations();
     }
 
+    /**
+     * Loads all character animations from the playerData object.
+     */
     loadAnimations() {
         const animationData = playerData.animations;
 
@@ -34,50 +44,74 @@ export class Player extends GameObject {
             this.animations[animation] = [];
 
             for (let i = 0; i < animationData[animation].numSprites; i++) {
-                this.animations[animation].push(new Sprite(`${animationData[animation].path}/${i}.png`).image);
+                const spriteImage = new Sprite(`${animationData[animation].path}/${i}.png`).image;
+                this.animations[animation].push(spriteImage);
             }
         }
     }
 
+    /**
+     * Sets the animation based on the velocity and isAttacking state.
+     */
     setAnimation() {
         const direction = this.isLastInputRight ? 'right' : 'left';
 
-        if (this.velocity.x == 0 && this.velocity.y == 0) {this.currentAnimation = `idle_${direction}`}
-        if (this.velocity.x > 0 && this.velocity.y == 0) {this.currentAnimation = `run_right`}
-        if (this.velocity.x < 0 && this.velocity.y == 0) {this.currentAnimation = `run_left`}
-        if (this.velocity.y < 0) {this.currentAnimation = `jump_${direction}`}
-        if (this.velocity.y > 0) {this.currentAnimation = `fall_${direction}`}
-        if (this.isAttacking) {this.currentAnimation = `attack_${direction}`}
-        if (this.isAttacking && this.animationFrame === 3) {this.isAttacking = false}
+        if (this.velocity.x == 0 && this.velocity.y == 0) { this.currentAnimation = `idle_${direction}` }
+        if (this.velocity.x > 0 && this.velocity.y == 0) { this.currentAnimation = `run_right` }
+        if (this.velocity.x < 0 && this.velocity.y == 0) { this.currentAnimation = `run_left` }
+        if (this.velocity.y < 0) { this.currentAnimation = `jump_${direction}` }
+        if (this.velocity.y > 0) { this.currentAnimation = `fall_${direction}` }
+        if (this.isAttacking) { this.currentAnimation = `attack_${direction}` }
+        this.resetAttackState();
     }
 
-    playAnimation(name) {
+    /**
+     * Renders the sprite of the currentAnimation based on the animationFrame.
+     * @param {String} name Name of the animation.
+     */
+    renderAnimation(name) {
         const animationArr = this.animations[name];
-        const currentFrame = globalThis.frameCounter;
+        const spriteId = this.animationFrame % animationArr.length;
         const spriteCorrectionX = playerData.spriteCorrection.x;
         const spriteCorrectionY = playerData.spriteCorrection.y;
 
-        if ((currentFrame % this.staggerFrames) == 0) {this.animationFrame++}
-        this.ctx.drawImage(animationArr[this.animationFrame % animationArr.length], this.position.x - spriteCorrectionX, this.position.y - spriteCorrectionY);
+        this.ctx.drawImage(animationArr[spriteId], this.position.x - spriteCorrectionX, this.position.y - spriteCorrectionY);
+        this.increaseAnimationFrame();
     }
 
+    /**
+     * Increases the current animation frame based on the staggerFrames value.
+     * Animations can be slowed down by increasing the staggerFrames value and vice versa.
+     */
+    increaseAnimationFrame() {
+        const currentFrame = globalThis.frameCounter;
+
+        if ((currentFrame % this.staggerFrames) == 0) { this.animationFrame++ }
+    }
+
+    /**
+     * Gets player input and sets velocity for movement or triggers an action such as jump or attack.
+     */
     getInput() {
         const input = this.inputController.getPressedKeys();
 
         // Left and Right
-        if (input.a) {this.velocity.x = -this.speed; this.isLastInputRight = false}
-        if (input.d) {this.velocity.x = this.speed; this.isLastInputRight = true}
-        if (input.a && input.d) {this.velocity.x = 0}
-        if (!input.a && !input.d) {this.velocity.x = 0}
-        
+        if (input.a) { this.velocity.x = -this.speed; this.isLastInputRight = false }
+        if (input.d) { this.velocity.x = this.speed; this.isLastInputRight = true }
+        if (input.a && input.d) { this.velocity.x = 0 }
+        if (!input.a && !input.d) { this.velocity.x = 0 }
+
         // Jump
-        if (input.w) {this.jump()}
+        if (input.w) { this.jump() }
 
         // Attack
-        if (input.space) {this.attack()}
+        if (input.space) { this.attack() }
     }
 
-    jump() { 
+    /**
+     * Makes the character jump.
+     */
+    jump() {
         if (this.canJump && this.velocity.y <= 0) {
             this.velocity.y = 0;
             this.velocity.y -= this.jumpVelocity;
@@ -85,36 +119,75 @@ export class Player extends GameObject {
         }
     }
 
+    /**
+     * Lets the character attack.
+     */
     attack() {
         if (!this.isAttacking && !this.attackCooldown) {
             this.isAttacking = true;
             this.attackCooldown = true;
             this.animationFrame = 0;
-            setTimeout(() => {this.attackCooldown = false}, 500);
+            setTimeout(() => { this.attackCooldown = false }, playerData.attackCooldown);
         }
     }
 
+    /**
+     * Resets the attack state after the last frame is rendered.
+     */
+    resetAttackState() {
+        if (this.isAttacking && this.animationFrame === 3) { this.isAttacking = false }
+    }
+
+    /**
+     * Checks if the player is alive.
+     * @returns Returns a boolean if the player is alive.
+     */
+    isAlive() {
+        if (this.health > 0) { return true }
+        return false;
+    }
+
+    /**
+     * Constrains the player to the canvas and prevents the player from getting out of bounds.
+     */
     constraint() {
         const maxX = window.width - this.width;
         const maxY = window.height - this.height;
 
-        if (this.position.x < 0) {this.position.x = 0}
-        if (this.position.y < 0) {this.position.y = 0}
-        if (this.position.x > maxX) {this.position.x = maxX}
-        if (this.position.y > maxY) {this.position.y = maxY}
+        if (this.position.x < 0) { this.position.x = 0 }
+        if (this.position.y < 0) { this.position.y = 0 }
+        if (this.position.x > maxX) { this.position.x = maxX }
+        if (this.position.y > maxY) { this.position.y = maxY }
     }
 
-    update(terrainSprites) {
+    /**
+     * Updates the character.
+     * @param {Object} collisionGroup Object of the sprites the player can collide with.
+     */
+    update(collisionGroup) {
         this.getInput();
         this.move();
-        this.checkCollisions('horizontal', terrainSprites);
+
+        // -----------------------------------------------------------------
+        this.checkCollisions(collisionGroup.terrainSprites, 'horizontal');
         super.addGravity();
-        this.checkCollisions('vertical', terrainSprites);
+        this.checkCollisions(collisionGroup.terrainSprites, 'vertical');
+        if (this.checkCollisions(collisionGroup.waterSprites, 'water')) {
+            this.position.x = this.startPosition.x;
+            this.position.y = this.startPosition.y;
+            this.health -= 1;
+        };
+        if (!this.isAlive()) { }
+        // -----------------------------------------------------------------
+        
         this.constraint();
+        this.setAnimation();
     }
 
+    /**
+     * Renders the character sprite.
+     */
     render() {
-        this.setAnimation();
-        this.playAnimation(this.currentAnimation);
+        this.renderAnimation(this.currentAnimation);
     }
 }
