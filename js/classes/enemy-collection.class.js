146df9c660b4b4d6b05b176d6e9ba092dae540f0
Dennis Ammen <dennis.ammen@outlook.com>
2023-01-08 14:58:46 +0100

    Added enemy collection class

---
+++ js/classes/enemy-collection.class.js
@@ -0,0 +1,57 @@
+import { Enemy } from './enemy.class.js';
+import { levelSize, tileSize } from './settings.js';
+
+/**
+ * Collection of enemies.
+ */
+export class EnemyCollection {
+    constructor(ctx, data) {
+        this.ctx = ctx;
+        this.data = data;
+        this.enemies = [];
+
+        this.loadEnemies();
+    }
+
+    /**
+     * Load the enemies based on the layer data.
+     */
+    loadEnemies() {
+        const levelWidth = levelSize.width;
+        let row = 0;
+
+        for (let i = 0; i < this.data.length; i++) {
+            const col = i % levelWidth;
+            const dataId = this.data[i];
+
+            if (this.data[i] != 0) {
+                const xPosition = col * tileSize.width;
+                const yPosition = row * tileSize.height;
+                const enemy = new Enemy(this.ctx, this.data, {x: xPosition, y: yPosition});
+
+                this.enemies.push(enemy);
+            }
+
+            if (col === (levelWidth - 1)) { row += 1 }
+        }
+    }
+
+    /**
+     * Updates the enemies array.
+     * @param {Object} collisionGroup Collision group with sprites of each layer.
+     */
+    update(collisionGroup) {
+        for (let enemy of this.enemies) {
+            enemy.update(collisionGroup);
+        }
+    }
+
+    /**
+     * Renders the enemies array.
+     */
+    render() {
+        for (let enemy of this.enemies) {
+            enemy.render();
+        }
+    }
+}
