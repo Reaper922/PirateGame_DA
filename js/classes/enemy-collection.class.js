881f62c9acc05b8fc640730fb9987702e52e2ac6
Dennis Ammen <dennis.ammen@outlook.com>
2023-01-30 22:51:41 +0100

    Modified audio and update function

--- js/classes/enemy-collection.class.js
+++ js/classes/enemy-collection.class.js
@@ -5,10 +5,12 @@ import { levelSize, tileSize } from './settings.js';
  * Collection of enemies.
  */
 export class EnemyCollection {
-    constructor(ctx, data) {
+    constructor(ctx, data, player) {
         this.ctx = ctx;
         this.data = data;
+        this.player = player;
         this.enemies = [];
+        this.enemyHitAudio = new Audio("./assets/sounds/enemy_hit.wav");
 
         this.loadEnemies();
     }
@@ -24,7 +26,7 @@ export class EnemyCollection {
             const col = i % levelWidth;
             const dataId = this.data[i];
 
-            if (this.data[i] != 0) {
+            if (dataId != 0) {
                 const xPosition = col * tileSize.width;
                 const yPosition = row * tileSize.height;
                 const enemy = new Enemy(this.ctx, this.data, {x: xPosition, y: yPosition});
@@ -36,11 +38,35 @@ export class EnemyCollection {
         }
     }
 
+    playAudio() {
+        this.enemyHitAudio.muted = globalThis.muteGameSound;
+        this.enemyHitAudio.currentTime = 0;
+        this.enemyHitAudio.volume = 0.6;
+        this.enemyHitAudio.play();
+    }
+
     /**
      * Updates the enemies array.
      * @param {Object} collisionGroup Collision group with sprites of each layer.
      */
     update(collisionGroup) {
+        if (this.player.isAttacking) {
+            for (let enemy of this.enemies) {
+                const attackRect = this.player.getAttackRect();
+                const collision = {
+                    top: enemy.position.y < attackRect.position.y + attackRect.size.height,
+                    bottom: enemy.position.y + enemy.height > attackRect.position.y,
+                    left: enemy.position.x < attackRect.position.x + attackRect.size.width,
+                    right: enemy.position.x + enemy.width > attackRect.position.x
+                }
+    
+                if (collision.top && collision.bottom && collision.left && collision.right) {
+                    this.playAudio();
+                    this.enemies.splice(this.enemies.indexOf(enemy), 1);
+                }
+            }
+        }
+
         for (let enemy of this.enemies) {
             enemy.update(collisionGroup);
         }
