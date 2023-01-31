2025643439a1a6a6303245a5094069bce76ae46b
Dennis Ammen <dennis.ammen@outlook.com>
2023-01-31 22:08:02 +0100

    Moved mute and fullscreen functions to game class

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 import { Game } from './classes/game.class.js';
 
 
-const muteBtn = document.getElementById('mute');
-const fullscreenBtn = document.getElementById('fullscreen');
-globalThis.muteGameSound = false;
-
-
 /**
  * Initial function that gets executed when the page is loaded.
  */
@@ -16,55 +11,14 @@ function init() {
 
 
 /**
- * Function that toggles mute.
- */
-function toggleMute() {
-  if (!globalThis.muteGameSound) {
-    globalThis.muteGameSound = true;
-  } else {
-    globalThis.muteGameSound = false;
-  }
-}
-
-
-/**
- * Event listener to toogle mute on click.
- */
-muteBtn.addEventListener('click', () => {
-  muteBtn.blur();
-  toggleMute();
-});
-
-
-/**
- * Function that toggles the fullscreen mode.
- */
-function toggleFullScreen() {
-  if (!document.fullscreenElement) {
-    document.getElementById('container').requestFullscreen();
-  } else if (document.exitFullscreen) {
-    document.exitFullscreen();
-  }
-}
-
-
-/**
- * Event listener to toogle fullscreen on click.
- */
-fullscreenBtn.addEventListener('click', () => {
-  fullscreenBtn.blur();
-  toggleFullScreen();
-});
-
-
-/**
  * Event listener to position the custom cursor on mouse movement.
  */
 addEventListener('mousemove', (event) => {
   const cursor = document.getElementById('cursor');
+  const offset = 2;
 
-  cursor.style.top = `${(event.y + 2).toFixed().toString()}px`;
-  cursor.style.left = `${(event.x + 2).toFixed().toString()}px`;
+  cursor.style.top = `${(event.y + offset).toFixed().toString()}px`;
+  cursor.style.left = `${(event.x + offset).toFixed().toString()}px`;
 });
 
 
