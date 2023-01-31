91da46a1de630008126f4a158af9630999ad5587
Dennis Ammen <dennis.ammen@outlook.com>
2023-01-31 23:35:18 +0100

    Added JsDoc comments

--- js/classes/audio.class.js
+++ js/classes/audio.class.js
@@ -1,12 +1,18 @@
+/**
+ * Audio class that represents a game sound.
+ */
 export class GameAudio {
-    constructor(path, volume = 1, restart = false) {
+    constructor(path, volume = 1, restartOnPlay = false) {
         this.audio = new Audio(path);
         this.audio.volume = volume;
-        this.restart = restart;
+        this.restartOnPlay = restartOnPlay;
     }
     
+    /**
+     * Plays the audio.
+     */
     play() {
-        if (this.restart) {this.audio.currentTime = 0}
+        if (this.restartOnPlay) {this.audio.currentTime = 0}
         this.audio.muted = globalThis.muteGameSound;
         this.audio.play();
     }
